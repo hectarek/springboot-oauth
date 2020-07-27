8f571afc6284be3badac201375d88546c33fd924
Hector Gonzalez <hectarek13@gmail.com>
2020-07-27 11:47:33 -0400

    first commit

---
+++ src/test/java/com/spingoauth/springoauth/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingoauth.springoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
